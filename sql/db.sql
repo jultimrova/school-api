80b33a9b3d06e72e81861d473175377a42c0b994
jultimrova@gmail.com <splntrs019>
2021-01-03 17:20:01 +0200

    start server, create db connection and first table

---
+++ sql/db.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS school_db;
+CREATE DATABASE IF NOT EXISTS school_db;
+
+USE school_db;
+
+DROP TABLE IF EXISTS teacher;
+
+CREATE TABLE IF NOT EXISTS teacher(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(1) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    subject_taught VARCHAR(100) NOT NULL,
+    years_of_experience INT(100) NOT NULL
+);
+
+DESCRIBE teacher;
---
+++ src/db.ts
@@ -0,0 +1,13 @@
+import { createPool } from 'mysql2/promise';
+
+export default async function connect() {
+  const connection = await createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '1111',
+    database: 'school_db',
+    connectionLimit: 10,
+  });
+
+  return connection;
+}
