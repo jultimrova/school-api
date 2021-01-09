925d9a58c11034794cdf41b9b16bd73fa7f66dc6
jultimrova@gmail.com <splntrs019>
2021-01-09 17:35:19 +0200

    env file added

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import App from './app';
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
+require('dotenv').config()
+
 async function main() {
   const app = new App(SERVER_PORT);
   await app.listen();
